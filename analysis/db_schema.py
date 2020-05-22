2575c63f668b0832c5ca7843990271af1ec6aba6
Eric Vin <ericvin@outlook.com>
2020-05-21 19:11:12 -0700

    Updated Schema and added Documentation

--- analysis/db_schema.py
+++ analysis/db_schema.py
@@ -7,6 +7,8 @@
 # The Wikipedia DB is documented at
 # https://upload.wikimedia.org/wikipedia/commons/9/94/MediaWiki_1.28.0_database_schema.svg
 
+# Modified by Eric Vin, 2020
+
 from .configuration import config
 import datetime
 import traceback
@@ -24,63 +26,95 @@ def define_tables(uri, migrate_enabled = False, fake_migrate_all=False):
 
     old_date = datetime.datetime(year=1980, month=1, day=1)
 
+    ### Wikipedia Data Tables ###
+
+    db.define_table(
+        # Environment is a group of pages that are linked such that reputation
+        # is shared between them.
+        'environment',
+
+        # Unique id for the environment
+        Field('environment_id', 'integer'),
+
+        # Plain text name for the environment
+        Field('environment_name'),
+    )
+
     db.define_table(
         'page',
+
         Field('page_id', 'integer'),
         Field('page_title'),
+
+        # A reference to the environment the page is a part of
+        Field('page_environment', 'reference environment'),
+
         # We want to analyze this page only from this date onwards.  If None, then
         # analyze from the beginning.
         Field('analysis_start_time', 'datetime'),
     )
 
     db.define_table(
-        'revision',
-        Field('rev_id', 'integer'),
-        # In which block it is.
-        Field('block_id', 'reference revision_block'),
-        # user id on the wikipedia for this user.
+        'user',
+
+        # The user_id is the same as on the wikipedia.
         Field('user_id', 'integer'),
+        Field('user_name'),
+        Field('user_real_name'),
+    )
+
+    db.define_table(
+        'revision',
+
+        Field('rev_id', 'integer'),
+
+        # Reference to user who made the revision.
+        Field('user', 'reference user'),
+
         Field('rev_date', 'datetime'),
         Field('rev_page', 'reference page'),
-        # ID of a blob in GCS or S3 where the text can be found.
-        Field('rev_text'),
+
+        # File name for a the compressed block in GCS and the offset where
+        # the revision is present inside that block.
+        Field('rev_file_name'),
+        Field('rev_file_offset'),
+
+        # NOTE: Plan to use static caching for reputation-annotated text instead, as
+        # it is never used in any other calculations.
         # ID of a blob in GCS or S3 where the reputation-annotated text can be found.
-        Field('annotated_text'),
+        # Field('annotated_text'),
+
         # Date at which the annotation has been computed, used to estimate whether
         # it should be recomputed.
         Field('annotation_date', 'datetime'),
     )
 
-    db.define_table(
-        'user',
-        # The user_id is the same as on the wikipedia.
-        Field('user_id', 'integer'),
-        Field('user_name'),
-        Field('user_real_name'),
-        # Value of reputation
-        Field('reputation', 'double'),
-    )
 
-    db.define_table(
-        'revision_block',
-        Field('page_id', 'reference page'),
-        # These fields are used to know in which order to stitch blocks
-        # together, and also, if there are holes between them.
-        # Id of the last revision before the block
-        Field('prev_revision_id', 'integer'), # long??
-        Field('next_revision_id', 'integer'),
-        # To put blocks in chronological order by page.
-        Field('initial_timestamp', 'datetime'),
-        Field('storage_id'), # ID for S3 / GCS
-    )
+    ### User Reputation Tables ###
 
     db.define_table(
         'user_reputation',
-        Field('user_id', 'reference user'),
-        Field('block_id', 'reference revision_block'),
-        Field('amount', 'double'),
+
+        # The user in question.
+        Field('user', 'reference user'),
+
+        #The environment in which this reputation applies.
+        Field('environment', 'reference environment'),
+
+        Field('reputation', 'double'),
     )
 
+    ### Cache tables ###
+
+    db.define_table(
+        'edit_distance',
+
+        # Note: revision_id_1 < revision_id_2
+        Field('revision_id_1', 'integer'),
+        Field('revision_id_2', 'integer'),
+
+        Field('distance', 'double'),
+    )
 
 
 
