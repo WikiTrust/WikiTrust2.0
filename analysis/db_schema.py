05949a3df645f28c7c3ea54e9f3d7ded04143f74
Luca de Alfaro <luca@dealfaro.org>
2020-06-23 16:27:30 -0700

    end of Vin meeting

--- analysis/db_schema.py
+++ analysis/db_schema.py
@@ -34,16 +34,65 @@ def define_tables(uri, migrate_enabled = False, fake_migrate_all=False):
         Field('page_id', 'integer'), # On wikipedia
         Field('environment_id', 'reference environment', ondelete="SET NULL"),
         Field('page_title'),
-        Field('analysis_time', 'datetime'),
+        Field('last_check_time', 'datetime'), # Of last check on Wikipedia,
+        # there may be more revisions after this date that we do not know about.
     )
 
     db.define_table(
         'revision',
-        Field('rev_id', 'integer'), # On wikipedia
+        Field('revision_id', 'integer'), # On wikipedia
         # user id on the wikipedia for this user.
         Field('user_id', 'integer'),
-        Field('rev_date', 'datetime'),
-        Field('rev_page', 'reference page'),
+        Field('revision_date', 'datetime'),
+        Field('page_id', 'integer'), # On wikipedia
+        Field('revision_page', 'reference page'),
+        Field('revision_blob'), # On GCS
+        Field('has_text', 'boolean'),
+        Field('num_attempts', 'integer'), # To get markup from Wikipedia
+        Field('last_attempt_date', 'datetime'), # Can also be a successful attempt.
+    )
+
+    db.define_table(
+        'revision_log', # Stores what has been done on a revision
+        Field('page', 'reference page'),
+        Field('algorithm'), # Which algo: text diff, auth rep, text rep, authorship, ... with version
+        Field('last_revision', 'reference revision'), # Analyzed.
+        Field('lock_date', 'datetime'), # if null or old the page is not locked.
+    )
+
+    db.define_table(
+        'text_storage',
+        Field('kind'), # markup, markup_w_rep, markup_w_author, ...
+        Field('revision_id'),
+        Field('page_id'),
+        Field('revision_date', 'datetime'),
+        Field('version'),
+        Field('blob'), # name of GCS blob where info is stored.
+    )
+
+    db.define_table(
+        'triangles',
+        Field('page', 'reference page'),
+        Field('algorithm'),
+        Field('info', 'text'),
+        # Json containing, for each previous revision p and subsequent revision f,
+        # the distances in the triangle, and the user_ids of the authors.
+        # {'revisions': [34, 36, 37], 'distances': [4.5, 5, 6.7], 'authors': [4, 5, 8], }
+        Field('triangle_date', 'datetime'), # of last revision (check)
+        Field('reputation_inc', 'double'), # How much the reputation was incremented
+        # as a consequence of the triangle.  Set to null if triangle not processed.
+        # This enables triangles to be processed twice. (check that we can query for null numbers).
+    )
+
+    # It is a good idea to cache the output of chdiff between consecutive revisions. 
+
+    db.define_table(
+        'reputation',
+        Field('user', 'reference user'),
+        Field('environment', 'reference environment'),
+        Field('algorithm'),
+        Field('reputation_value', 'double'),
+
     )
 
     db.define_table(
@@ -57,36 +106,12 @@ def define_tables(uri, migrate_enabled = False, fake_migrate_all=False):
     )
 
     db.define_table(
-        'revision_block',
-        Field('page_id', 'reference page'),
-        # These fields are used to know in which order to stitch blocks
-        # together, and also, if there are holes between them.
-        # Id of the last revision before the block
-        Field('prev_revision_id', 'integer'), # long??
-        Field('next_revision_id', 'integer'),
-        # To put blocks in chronological order by page.
-        Field('initial_timestamp', 'datetime'),
-        Field('storage_id'), # ID for S3 / GCS
-    )
-
-    db.define_table(
-        'text_storage',
-        Field('kind'), # markup, markup_w_rep, markup_w_author, ...
-        Field('revision_id'),
-        Field('page_id'),
-        Field('blob_id'), # pointer to GCS or S3: name of the blob where the revision can be found.
-    )
-
-    db.define_table(
         'user_reputation',
         Field('user_id', 'reference user'),
         Field('topic id', 'reference topic', ondelete="SET NULL"),
         Field('block_id', 'reference revision_block'),
         Field('amount', 'double'),
     )
-
-
-
 
 
 def create_indices(db):
