f615e2ffbaec493a05743a9aa424cb424f629997
Luca de Alfaro <luca@dealfaro.org>
2020-07-03 14:51:10 -0700

    minor changes

--- analysis/db_schema.py
+++ analysis/db_schema.py
@@ -38,7 +38,7 @@ def define_tables(uri, migrate_enabled = False, fake_migrate_all=False):
     db.define_table(
         'page',
         Field('page_id', 'integer'), # On wikipedia
-        
+
         Field('environment_id', 'reference environment', ondelete="SET NULL"),
 
         Field('page_title'),
@@ -53,45 +53,45 @@ def define_tables(uri, migrate_enabled = False, fake_migrate_all=False):
         Field('user_id', 'integer'), # The user_id is the same as on the wikipedia.
 
         Field('user_name'),
-        
+
         Field('user_real_name'), #ASK LUCA, Is this needed?
     )
 
     db.define_table(
         'revision',
         Field('revision_id', 'integer'), # On Wikipedia
-        
+
         Field('user_id', 'integer'), # User id on the wikipedia for this user.
 
-        Field('revision_date', 'datetime'), 
-        
-        Field('page_id', 'integer'), # On wikipedia 
+        Field('revision_date', 'datetime'),
+
+        Field('page_id', 'integer'), # On wikipedia
         #ASK LUCA, does this need to be stored here if it is in Page table?
         #Avoids cost of 1 join but duplicate
-        
-        Field('revision_page', 'reference page'), 
-        
+
+        Field('revision_page', 'reference page'),
+
         Field('revision_blob'), # On GCS
-        
+
         Field('text_retrieved', 'boolean'), #True means retrieved, False means not retrieved
-        
+
         Field('num_attempts', 'integer'), # To get markup from Wikipedia
-        
+
         Field('last_attempt_date', 'datetime'), # Can also be a successful attempt.
     )
 
     # Stores what has been done on a revision and whether or not it is locked
     db.define_table(
         'revision_log',
-    
+
         Field('page', 'reference page'),
-    
-        Field('algorithm'), # Which algorithm this log is in reference to. 
+
+        Field('algorithm'), # Which algorithm this log is in reference to.
         #(Text diff, Edit distance, Author rep, etc...)
-    
+
         Field('last_revision', 'reference revision'), # Last revision analyzed.
         #I.E. How far we've gotten
-    
+
         Field('lock_date', 'datetime'), # If null or old the page is not locked.
     )
 
@@ -103,10 +103,10 @@ def define_tables(uri, migrate_enabled = False, fake_migrate_all=False):
 
         # The environment in which this reputation applies.
         Field('environment', 'reference environment'),
-        
+
         # String identifier for algorithm that was run to determine this reputation
         Field('algorithm'),
-        
+
         Field('reputation_value', 'double'),
     )
 
@@ -130,19 +130,19 @@ def define_tables(uri, migrate_enabled = False, fake_migrate_all=False):
     # Cache for edit distance "triangles"
     db.define_table(
         'triangles',
-        
+
         Field('page', 'reference page'),
-        
+
         Field('algorithm'),
-        
+
         Field('info', 'text'),
         # Json containing, for each previous revision p and subsequent revision f,
         # the distances in the triangle, and the user_ids of the authors.
         # {'revisions': [34, 35, 37], 'distances': [4.5, 5, 6.7], 'authors': [4, 5, 8], }
 
-        Field('judged_revision', 'integer'), # Revision id of middle revision
-
-        Field('new_version', 'integer'), # Revision id of last revision (Not sure if needed?)
+        Field('revid_1', 'integer'),
+        Field('revid_2', 'integer'),
+        Field('revid_3', 'integer'),
 
         Field('reputation_inc', 'double'), # How much the reputation was incremented
         # as a consequence of the triangle.  Set to null if triangle not processed.
@@ -156,12 +156,12 @@ def define_tables(uri, migrate_enabled = False, fake_migrate_all=False):
 
         Field('origin_revision_id', 'integer'),
 
-        Field('dest_revision_id', 'integer'), 
-        
+        Field('dest_revision_id', 'integer'),
+
         #Note: origin_revision_id < dest_revision_id
 
-        Field('info', 'text') 
-        #Text version of text_diff tuples between origin revision and dest revision       
+        Field('info', 'text')
+        #Text version of text_diff tuples between origin revision and dest revision
 
     )
 
