9d07efafc1c2d43f0747a33c625891583606fae3
Eric Vin <ericvin@outlook.com>
2020-08-28 11:26:59 -0700

    Completed local_storage_engine

--- analysis/local_storage_engine.py
+++ analysis/local_storage_engine.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 class LocalStorageEngine(object):
     """
@@ -13,7 +14,9 @@ class LocalStorageEngine(object):
         :param database_table:
         :param default_version:
         """
-        pass
+
+        #Initialize a dictionary mapping page_id to a page_dictionary
+        self.pages = {}
 
     def store(self, page_id: int, version_id: str, rev_id: int, text: str, timestamp: datetime.datetime):
         """Writes to the store.
@@ -25,26 +28,45 @@ class LocalStorageEngine(object):
         :return: a boolean indicating whether all has been written (True) or whether some
             changes are pending (False) for the given page_id and version_id.
         """
-        pass
+        if page_id not in self.pages:
+            self.pages[page_id] = {}
+
+        self.pages[page_id][rev_id] = text
 
 
     def read(self, page_id: int, version_id: str, rev_id: int) -> str:
         """
         Reads from the text storage.
-        :param page_id:
-        :param version_id:
-        :param rev_id:
+        :param page_id: id of page (or in general, of compression space)
+        :param version_id: id of the version we are writing.
+        :param rev_id: id of revision (or in general, of object in space)
         :return: The string that was written.
         """
-        pass
+        assert page_id in self.pages
+        assert rev_id in self.pages[page_id]
+
+        return self.pages[page_id][rev_id]
 
 
     def flush(self, page_id: int, version_id: str):
         """Writes all remaining changes to the given page_id and version_id."""
         pass
 
-def load_json_into_storage(storage_engine, json):
+def load_page_json_into_storage(storage_engine, json):
     """
-    Takes a JSON containing information about a Wikipedia page and loads it into the storage engine.
+    Takes a JSON object containing information about a Wikipedia page and loads it into the storage engine.
     """
-    pass
+    page_id = int(json["pageId"])
+
+    for rev_iter in range(int(json["size"])):
+        rev_id = json["revisions"][rev_iter]["revisionId"]
+        rev_text = json["revisions"][rev_iter]["text"]
+        storage_engine.store(page_id, "DUMMY_VERSION", rev_id, rev_text, datetime.datetime.now())
+
+    print(dummy_storage_engine.read(page_id, "DUMMY_VERSION", 429097598))
+if __name__ == '__main__':
+    dummy_storage_engine = LocalStorageEngine(db = None)
+
+    with open("/home/ericvin/Projects/WikiTrust2.0/LadyGagaMeatDressRevisions/all_revision.json") as json_file:
+        json_object = json.load(json_file)
+        load_page_json_into_storage(dummy_storage_engine, json_object)
