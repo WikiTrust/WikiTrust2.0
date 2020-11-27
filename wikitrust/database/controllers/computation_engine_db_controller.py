e1f91dbfea4285ef9d20e6c94ec95035be2b4219
LukeJHarvey <wxisxawesome@gmail.com>
2020-11-27 14:20:24 -0800

    new functions created for computation

--- wikitrust/database/controllers/computation_engine_db_controller.py
+++ wikitrust/database/controllers/computation_engine_db_controller.py
@@ -23,24 +23,27 @@ class computation_engine_db_controller:
             prev2 = prev.rev_id
         print("Previous Revision Field Populated")
     
+    #+
     #parameters: rev_id
     #return previous revision id
     def get_prev_rev(self, rev_id):
         rev = self.db(self.db.revision.rev_id == rev_id).select().first()
         return rev.prev_rev
 
+    #+
     #parameters: version, user_id, environment
     #return reputation of user
     def get_reputation(self, version, user_id, env):
         x = self.db.user_reputation.version == version
         y = self.db.user_reputation.user_id == user_id
         z = self.db.user_reputation.environment == env
-        user_rep = self.db(x & y & z).select().first()
+        user_rep = self.db(x & y & z).select(self.db.user_reputation.reputation_value).first()
         return user_rep.reputation_value
+
 
     #return unique id
     #update text Distances 1 - 2, 2 - 3, 1 - 3
-    def update_or_insert_triangle(self, version, page_id, revs, distances, rep = None):
+    def create_triangle(self, version, page_id, revs, distances, rep = None):
         v = self.db.triangles.version == version
         w = self.db.triangles.page_id == page_id
         x = self.db.triangles.rev_id_1 == revs[0]
@@ -61,13 +64,23 @@ class computation_engine_db_controller:
         #rev 1 to rev 3 distance
         self.create.create_text_distance(version, revs[0], revs[2], distances[2])
         return ret
+    
+    #parameters: triangle.id, reputation
+    #return: triangle.id
+    def update_triangle_rep(self, id, rep):
+        triangle = self.db(self.db.triangles.id == id).select().first()
+        triangle.repuatation_inc = rep
+        triangle.update_record()
+        self.db.commit()
+        return id
+        
 
     #parameters: version, page_id
-    #return all triangles in chronological order
+    #return all triangle id's (in chronological order by rev_id_2)
     def get_all_triangles_chronological(self, version, page_id):
         x = self.db.triangles.page_id == page_id
         y = self.db.triangles.version == version
-        all_triangles = self.db(x & y).iterselect(orderby=self.db.triangles.rev_id_2)
+        all_triangles = self.db(x & y).select(self.db.triangles.id).iterselect(orderby=self.db.triangles.rev_id_2)
         return list(all_triangles)
     
     #parameters: page_id
@@ -103,16 +116,24 @@ class computation_engine_db_controller:
 
     #parameters: version, page_id
     #return: all unprocessed triangles, in chronological order, by third revision, for a given page
+    #return only triangle unique ids
     def get_all_unprocessed_triangles(self, version):
         x = self.db.triangles.version == version
         y = self.db.triangles.reputation_inc == None
         unprocessed_triangles = self.db(x & y).iterselect(orderby=self.db.triangles.rev_id_2)
         return unprocessed_triangles
     
+    #+
     #parameters: page_id
     #return: environment
     def get_environment_by_page_id(self, page_id):
-        x = self.db.page.page_id = page_id
-        return self.db(x).environment_id
+        x = self.db.page.page_id == page_id
+        return self.db(x).select(self.db.page.environment_id).first().environment_id
 
-    #
+    #parameters: triangle.id
+    #return: 3 tuples with three tuples inside
+    # (rev1,rev2,rev3),(user_id1,...,...),(distance 1-2, distance 2-3, distance 1-3)
+    def get_triangle_info(self, id):
+        triangle = self.db(self.db.triangles.id == id).select().first()
+        rev_tup = (triangle.rev_id_1,triangle.rev_id_2,triangle.rev_id_3)
+        user1 = self.db(self.db.user)
