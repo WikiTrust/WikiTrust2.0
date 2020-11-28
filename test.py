89752b4adda2601829027d567428fa74d02c4736
Eric Vin <ericvin@outlook.com>
2020-11-27 16:06:47 -0800

    Completed test

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import wikitrust.test.db_test as db_test
+import wikitrust.test.computation_engine_debug as ce_test
+
 import wikitrust.database.db_schema as db_schema
 
 def db_population_test():
     db = db_schema.connect_to_db('sqlite://storage.sqlite')
     db_test.populate(db)
+
+if __name__ == '__main__':
+    ce_test.test_computation_engine()
--- wikitrust/test/computation_engine_debug.py
+++ wikitrust/test/computation_engine_debug.py
@@ -1,9 +1,11 @@
+import dbtest
+
 import wikitrust.database.controllers.computation_engine_db_controller as db_controller
 import wikitrust.storage_engine.local_storage_engine as local_storage_engine
 
-import wikitrust.computation_engine.triangle_generator as triangle_generator
-import wikitrust.computation_engine.reputation_generator as reputation_generator
-import wikitrust.computation_engine.text_annotation as text_annotation
+from wikitrust.computation_engine.triangle_generator import TriangleGenerator
+from wikitrust.computation_engine.reputation_generator import ReputationGenerator
+from wikitrust.computation_engine.text_annotation import TextAnnotation
 import wikitrust.computation_engine.wikitrust_algorithms.text_diff.chdiff as chdiff
 
 import json
@@ -12,11 +14,11 @@ import json
 __DBURI__ = "sqlite://Test.db"
 __PAGEID__ = 31774937
 __PAGEJSON__ = "../../LadyGagaMeatDressRevisions/all_revision.json"
-__ALGORITHM_VER__ = 0.1
+__ALGORITHM_VER__ = "0.1"
 
-if __name__ == '__main__':
+def test_computation_engine():
     # Initialize DB controller
-    
+    dbcontroller = dbtest.drop_and_populate(__DBURI__)
 
     # Initialize local text storage engine
     text_storage_engine = local_storage_engine.LocalStorageEngine(db = None)
@@ -29,10 +31,12 @@ if __name__ == '__main__':
     trust_storage_engine = local_storage_engine.LocalStorageEngine(db = None)
 
     #Run Triangle generator
-    tg = triangle_generator.TriangleGenerator(db, text_storage_engine, "0.1", 10, chdiff.edit_diff_greedy, chdiff.make_index2)
+    tg = TriangleGenerator(dbcontroller, text_storage_engine, __ALGORITHM_VER__, (10, chdiff.edit_diff_greedy, chdiff.make_index2))
     tg.compute_triangles_batch(__PAGEID__)
 
     #Run Reputation generator
+    rg = ReputationGenerator(dbcontroller, __ALGORITHM_VER__, (1, (lambda x: x)))
+    rg.update_author_reputation()
 
     #Run Text Annotation on each revision
-
+    ta = TextAnnotation(dbcontroller, text_storage_engine, trust_storage_engine, __ALGORITHM_VER__, (0.5, 0.5, 5, chdiff.edit_diff_greedy, chdiff.make_index2))
